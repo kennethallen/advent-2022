908d5673e4665655482226cdd8f928f479b60924
Kenneth Allen <kenneth@kallen.dev>
2022-12-02 12:13:40 +1100

    Clean up 01

--- src/01.zig
+++ src/01.zig
@@ -12,31 +12,33 @@ fn order_u32(a: u32, b: u32) math.Order {
 }
 
 pub fn main() !void {
-  const file = try fs.cwd().openFile("src/01.txt", .{});
-  defer file.close();
-  const reader = file.reader();
-
-  var line = std.ArrayList(u8).init(alloc);
-  defer line.deinit();
-
   var max_elves = toplist.Toplist(u32, 3, order_u32) { .items = [_]u32{0} ** 3 };
   {
-    var elf: u32 = 0;
+    const file = try fs.cwd().openFile("src/01.txt", .{});
+    defer file.close();
+    const reader = file.reader();
+
+    var line = std.ArrayList(u8).init(alloc);
+    defer line.deinit();
+
+    var elf: ?u32 = null;
     while (true) {
-      reader.readUntilDelimiterArrayList(&line, '\n', 1_000_000)
+      reader.readUntilDelimiterArrayList(&line, '\n', 100)
         catch |err| switch (err) {
           error.EndOfStream => break,
           else => unreachable,
         };
       
-      if (std.fmt.parseUnsigned(u32, line.items, 0)) |n| {
-        elf += n;
-      } else |_| {
-        _ = max_elves.insert(elf);
-        elf = 0;
+      if (line.items.len == 0) {
+        if (elf) |e| {
+          _ = max_elves.insert(e);
+          elf = null;
+        }
+      } else {
+        elf = (elf orelse 0) + try std.fmt.parseUnsigned(u32, line.items, 0);
       }
     }
-    _ = max_elves.insert(elf);
+    if (elf) |e| { _ = max_elves.insert(e); }
   }
 
   const max_elf = max_elves.items[max_elves.items.len - 1];
