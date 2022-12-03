068d73973cba4f9a6b266deb17f0466d09587481
Kenneth Allen <kenneth@kallen.dev>
2022-12-03 17:06:54 +1100

    Use camel case

--- src/01.zig
+++ src/01.zig
@@ -5,12 +5,12 @@ const math = std.math;
 
 const toplist = @import("toplist.zig");
 
-fn order_u64(a: u64, b: u64) math.Order {
+fn orderU64(a: u64, b: u64) math.Order {
   return math.order(a, b);
 }
 
 pub fn main() !void {
-  var max_elves = toplist.Toplist(u64, 3, order_u64){};
+  var maxElves = toplist.Toplist(u64, 3, orderU64){};
   {
     const file = try fs.cwd().openFile("src/01.txt", .{});
     defer file.close();
@@ -22,17 +22,17 @@ pub fn main() !void {
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
       if (line.len == 0) {
         if (elf) |e| {
-          _ = max_elves.insert(e);
+          _ = maxElves.insert(e);
           elf = null;
         }
       } else
         elf = (elf orelse 0) + try std.fmt.parseUnsigned(u64, line, 0);
     }
-    if (elf) |e| _ = max_elves.insert(e);
+    if (elf) |e| _ = maxElves.insert(e);
   }
 
-  const max_elf = max_elves.asSlice()[max_elves.count - 1];
-  var sum_max_elves: u64 = 0;
-  for (max_elves.asSlice()) |elf| sum_max_elves += elf;
-  try std.io.getStdOut().writer().print("01 {} {}\n", .{ max_elf, sum_max_elves });
+  const maxElf = maxElves.asSlice()[maxElves.count - 1];
+  var sumMaxElves: u64 = 0;
+  for (maxElves.asSlice()) |elf| sumMaxElves += elf;
+  try std.io.getStdOut().writer().print("01 {} {}\n", .{ maxElf, sumMaxElves });
 }
