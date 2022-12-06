b4a06e64c0de985db6d16597697d1eb08ae2b82a
Kenneth Allen <kenneth@kallen.dev>
2022-12-07 00:06:05 +1100

    Solve day 06 part 2

--- src/06.zig
+++ src/06.zig
@@ -8,43 +8,48 @@ const Day06Error = error {
 };
 
 pub fn main() ![2]u64 {
-  var count: u64 = 4;
-  {
-    const file = try fs.cwd().openFile("input/06.txt", .{});
-    defer file.close();
-    const reader = file.reader();
-
-    var buf: [4]u8 = undefined;
-
-    if ((try reader.read(&buf)) < 4) {
-      return Day06Error.InvalidSignal;
-    }
-
-    while (true) {
-      if (buf[3] == '\n') {
-        return Day06Error.InvalidSignal;
-      }
-    
-      if (check(&buf)) {
-        break;
-      }
-
-      mem.copy(u8, buf[0..], buf[1..]);
-      buf[3] = try reader.readByte();
-      count += 1;
-    }
-  }
-
-  return .{ count, 0 };
+  return .{ try findMarker(4), try findMarker(14) };
 }
 
-pub fn check(buf: []u8) bool {
-  for (buf) |a, i| {
-    for (buf[(i+1)..]) |b| {
-      if (a == b) {
-        return false;
-      }
+fn findMarker(comptime window: usize) !u64 {
+  const file = try fs.cwd().openFile("input/06.txt", .{});
+  defer file.close();
+  const reader = file.reader();
+
+  var buf: [window]u8 = undefined;
+  var toAdvance: usize = window;
+  var total = toAdvance;
+
+  while (true) {
+    if (try reader.read(buf[buf.len - toAdvance..]) < toAdvance)
+      return Day06Error.InvalidSignal;
+    if (buf[buf.len - 1] == '\n')
+      return Day06Error.InvalidSignal;
+  
+    toAdvance = check(&buf, buf.len - toAdvance);
+    if (toAdvance == 0) break;
+
+    mem.copy(u8, buf[0..], buf[toAdvance..]);
+    total += toAdvance;
+  }
+
+  return @intCast(u64, total);
+}
+
+fn check(buf: []u8, split: usize) usize {
+  const lastNew = @max(split, 1);
+  var toAdvance: usize = 0;
+
+  var n = buf.len - 1;
+  while (n >= lastNew) : (n -= 1) {
+    var m = n - 1;
+    while (true) {
+      if (buf[n] == buf[m])
+        toAdvance = @max(toAdvance, m + 1);
+      if (m == 0) break;
+      m -= 1;
     }
   }
-  return true;
+
+  return toAdvance;
 }
