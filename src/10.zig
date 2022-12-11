19073624413c5a2eee6fe7476ec0428d8a8b5aff
Kenneth Allen <kenneth@kallen.dev>
2022-12-11 23:06:43 +1100

    Solve day 10 part 2

--- src/10.zig
+++ src/10.zig
@@ -19,6 +19,8 @@ pub fn main() ![2]u64 {
     defer file.close();
     const reader = file.reader();
 
+    const writer = io.getStdOut().writer();
+
     var buf: [100]u8 = undefined;
     var x: i64 = 1;
     var cycle: i64 = 0;
@@ -26,18 +28,29 @@ pub fn main() ![2]u64 {
 
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |row| {
       var nextX: i64 = x;
+      var nextCycle: i64 = cycle;
       if (mem.eql(u8, row, "noop"))
-        cycle += 1
+        nextCycle += 1
       else if (row.len >= 6 and mem.eql(u8, row[0..5], "addx ")) {
-        cycle += 2;
+        nextCycle += 2;
         nextX += try fmt.parseInt(i64, row[5..], 0);
       } else
         return Day10Error.InvalidInstruction;
-      
-      if (cycle >= nextAdd) {
-        sum += nextAdd * x;
-        nextAdd += 40;
+
+      while (cycle < nextCycle) {
+        const screenPos = @mod(cycle, 40);
+        const px = x >= screenPos - 1 and x <= screenPos + 1;
+        try writer.writeByte(if (px) '#' else ' ');
+        if (screenPos == 39) try writer.writeByte('\n');
+
+        cycle += 1;
+
+        if (cycle >= nextAdd) {
+          sum += nextAdd * x;
+          nextAdd += 40;
+        }
       }
+      
       x = nextX;
     }
   }
